0f34e5c8cc75bdd8d9800099e7bfb51bc66e5e0f
Louis <louis.dupraz@epitech.eu>
2023-06-19 00:06:37 +0200

    fix: fixed case where empty value was wrongly considered as edited NOTE: Registry editing part should now be functional. YAML files detection will be added soon

--- main.py
+++ main.py
@@ -79,11 +79,11 @@ def checkAndResetValue(path, value_name, original_value, datatype):
         value = getRegistryValue(key, value_name)
         if datatype == 'REG_BINARY':
             original_value = bytes.fromhex(original_value)
-        if str(value) != str(original_value) and input(f"The registery value {value_name} at {path} is set to {str(value)} instead of {str(original_value)}. Do you want to reset it? (y/n) ") == 'y':
+        if (str(value) != str(original_value) and (str(original_value) != "" and str(value) == "None")) and input(f"The registery value {value_name} at {path} is set to {str(value)} instead of {str(original_value)}. Do you want to reset it? (y/n) ") == 'y':
             print(f"Resetting registery value {value_name} at {path} to {original_value}")
 
-            print("DEV MODE, NOT ACTUALLY RESETING")
-            #setRegistryValue(key, value_name, original_value, datatype)
+            #print("DEV MODE, NOT ACTUALLY RESETING")
+            setRegistryValue(key, value_name, original_value, datatype)
             #TODO RESTORE FOR PROD
 
         reg.CloseKey(key)
@@ -96,8 +96,8 @@ def checkValueExistsAndDelete(path, value_name):
         if value is not None and input(f"The registery value {value_name} at {path} is set to {str(value)} but should have been removed. Do you want to delete it? (y/n) ") == 'y':
             print(f"Deleting registery value {value_name} at {path}")
 
-            print("DEV MODE, NOT ACTUALLY DELETING")
-            #reg.DeleteValue(key, value_name)
+            #print("DEV MODE, NOT ACTUALLY DELETING")
+            reg.DeleteValue(key, value_name)
             #TODO RESTORE FOR PROD
 
         reg.CloseKey(key)
@@ -154,6 +154,10 @@ def main():
     if len(args) != 1 or (args[0] == '-h' or args[0] == '--help'):
         print("Usage: python3 main.py <path to Atlas Playbook Directory>\nTo get the Atlas Playbook Directory, download the Atlas Playbook https://atlasos.net/ and extract it (password: malte)")
         exit(1)
+    path = args[0]
+    configpath = path + "\\Configuration\\features\\atlas\\config.yml"
+    configdata = readYamlFile(configpath)
+    processActions(configdata)
     exit(0)
 
 
