f97c5cc0cad047f4fa262b01f4b38ed8daa289ee
Louis <louis.dupraz@epitech.eu>
2023-06-18 17:27:03 +0200

    feat: added support for 'delete' operation

--- main.py
+++ main.py
@@ -45,12 +45,44 @@ def checkAndResetValue(path, value_name, original_value, datatype):
         value = getRegistryValue(key, value_name)
         if datatype == 'REG_BINARY':
             original_value = bytes.fromhex(original_value)
-        if str(value) != str(original_value) and input(f"The registery value {value_name} at {path} is set to {str(value)} instead of {original_value}. Do you want to reset it? (y/n) ") == 'y':
+        if str(value) != str(original_value) and input(f"The registery value {value_name} at {path} is set to {str(value)} instead of {str(original_value)}. Do you want to reset it? (y/n) ") == 'y':
             print(f"Resetting registery value {value_name} at {path} to {original_value}")
 
             print("WARNING: Registry datatypes not yet supported, cannot reset value")
 
             #setRegistryValue(key, value_name, original_value)
+        reg.CloseKey(key)
+
+
+def checkValueExistsAndDelete(path, value_name):
+    rootdir = path.split('\\')[0]
+    initial_key = None
+    match rootdir:
+        case 'HKCR': initial_key = reg.HKEY_CLASSES_ROOT
+        case 'HKEY_CLASSES_ROOT': initial_key = reg.HKEY_CLASSES_ROOT
+        case 'HKCU': initial_key = reg.HKEY_CURRENT_USER
+        case 'HKEY_CURRENT_USER': initial_key = reg.HKEY_CURRENT_USER
+        case 'HKLM': initial_key = reg.HKEY_LOCAL_MACHINE
+        case 'HKEY_LOCAL_MACHINE': initial_key = reg.HKEY_LOCAL_MACHINE
+        case 'HKU': initial_key = reg.HKEY_USERS
+        case 'HKEY_USERS': initial_key = reg.HKEY_USERS
+        case 'HKCG': initial_key = reg.HKEY_CURRENT_CONFIG
+        case 'HKEY_CURRENT_CONFIG': initial_key = reg.HKEY_CURRENT_CONFIG
+    if initial_key is None:
+        raise ValueError(f"Invalid root directory {rootdir}")
+    try:
+        key = reg.OpenKeyEx(initial_key, path[len(rootdir) + 1:] + '\\')
+    except PermissionError:
+        print(f"Permission denied to open {path}")
+        return
+    except FileNotFoundError:
+        print(f"Could not find key {path}")
+        return
+    if key:
+        value = getRegistryValue(key, value_name)
+        if value is not None and input(f"The registery value {value_name} at {path} is set to {str(value)} but should have been removed. Do you want to delete it? (y/n) ") == 'y':
+            print(f"Deleting registery value {value_name} at {path}")
+            reg.DeleteValue(key, value_name)
         reg.CloseKey(key)
 
 
@@ -79,7 +111,7 @@ def processActions(yaml_content):
             pass
         elif 'registryValue' in keys:
             if action['registryValue'].get('operation') == 'delete':
-                continue
+                checkValueExistsAndDelete(action['registryValue']['path'], action['registryValue']['value'])
             try:
                 checkAndResetValue(action['registryValue']['path'], action['registryValue']['value'],
                                    action['registryValue']['data'], action['registryValue']['type'])
