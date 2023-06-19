059cb415df8e9338e2b91f44d35ff0bae3865301
Louis <louis.dupraz@epitech.eu>
2023-06-19 10:39:43 +0200

    fix: fixed major bug

--- main.py
+++ main.py
@@ -79,11 +79,11 @@ def checkAndResetValue(path, value_name, original_value, datatype):
         value = getRegistryValue(key, value_name)
         if datatype == 'REG_BINARY':
             original_value = bytes.fromhex(original_value)
-        if (str(value) != str(original_value) and (str(original_value) != "" and str(value) == "None")) and input(f"The registery value {value_name} at {path} is set to {str(value)} instead of {str(original_value)}. Do you want to reset it? (y/n) ") == 'y':
+        if (str(value) != str(original_value) and not (str(original_value) == "" and str(value) == "None")) and input(f"The registery value {value_name} at {path} is set to {str(value)} instead of {str(original_value)}. Do you want to reset it? (y/n) ") == 'y':
             print(f"Resetting registery value {value_name} at {path} to {original_value}")
 
-            #print("DEV MODE, NOT ACTUALLY RESETING")
-            setRegistryValue(key, value_name, original_value, datatype)
+            print("DEV MODE, NOT ACTUALLY RESETING")
+            #setRegistryValue(key, value_name, original_value, datatype)
             #TODO RESTORE FOR PROD
 
         reg.CloseKey(key)
@@ -96,8 +96,8 @@ def checkValueExistsAndDelete(path, value_name):
         if value is not None and input(f"The registery value {value_name} at {path} is set to {str(value)} but should have been removed. Do you want to delete it? (y/n) ") == 'y':
             print(f"Deleting registery value {value_name} at {path}")
 
-            #print("DEV MODE, NOT ACTUALLY DELETING")
-            reg.DeleteValue(key, value_name)
+            print("DEV MODE, NOT ACTUALLY DELETING")
+            #reg.DeleteValue(key, value_name)
             #TODO RESTORE FOR PROD
 
         reg.CloseKey(key)
