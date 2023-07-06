824bf42bbbd414d215b2b31da44f799aa5319f97
Louis <louis.dupraz@epitech.eu>
2023-07-07 00:03:40 +0200

    feat: started working on services

--- src/services.py
+++ src/services.py
@@ -2,15 +2,14 @@ import win32serviceutil
 import win32service
 
 
-def list_all_services():
+def get_all_services():
     accessSCM = win32service.SC_MANAGER_ALL_ACCESS
     hscm = win32service.OpenSCManager(None, None, accessSCM)
     service_type = win32service.SERVICE_WIN32
     service_state = win32service.SERVICE_STATE_ALL
     status_handle = win32service.EnumServicesStatus(hscm, service_type, service_state)
-    for (short_name, desc, status) in status_handle:
-        print(f'{short_name} - {desc}')
     win32service.CloseServiceHandle(hscm)
+    return status_handle
 
 
 def delete_service(service_name):
@@ -23,3 +22,24 @@ def delete_service(service_name):
     except Exception as e:
         print(f'Failed to delete the service {service_name}: {str(e)}')
 
+
+def checkServiceExistsAndDelete(service_name, skip_prompts):
+    services = get_all_services()
+    services_names = {service[0] for service in services}
+    if service_name in services_names and (skip_prompts or input(f"The service {service_name} exists but should have been removed. Do you want to delete it? (y/n) ") == 'y'):
+        delete_service(service_name)
+
+
+def checkServiceStartupAndReset(service_name, startup_type, skip_prompts):
+    services = get_all_services()
+    services_names = {service[0] for service in services}
+    if service_name in services_names:
+        handle = win32serviceutil.SmartOpenService(None, service_name, win32service.SERVICE_ALL_ACCESS)
+        service_info = win32service.QueryServiceConfig(handle)
+        print(service_info[1], "||", win32service.SERVICE_AUTO_START, win32service.SERVICE_BOOT_START, win32service.SERVICE_DEMAND_START, win32service.SERVICE_DISABLED, win32service.SERVICE_SYSTEM_START)
+        return
+        if service_info[1] != startup_type and (skip_prompts or input(f"The service {service_name} is set to {service_info[1]} instead of {startup_type}. Do you want to reset it? (y/n) ") == 'y'):
+            pass
+
+
+checkServiceStartupAndReset("dcsvc", 1, False)
