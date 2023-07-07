d45e9e5e1903c0da2cbab3bfd1ee19ead781a904
Louis <louis.dupraz@epitech.eu>
2023-07-07 20:48:15 +0200

    fix: fix folder deletion command

--- src/task_scheduler.py
+++ src/task_scheduler.py
@@ -4,7 +4,7 @@ from subprocess import run
 def checkTasksFolderExistsAndDelete(path, skip_prompts):
     try:
         if skip_prompts or input(f"The tasks folder {path} exists but should have been removed. Do you want to delete it? (y/n) ") == 'y':
-            run(rf'schtasks /Delete /TN "{path}\*" /F', check=True, shell=True)
+            run(f'schtasks /Delete /TN {path}\\* /F', check=True, shell=True)
             run(f'schtasks /Delete /TN "{path}" /F', check=True, shell=True)
             print(f' ==> Deleting tasks folder {path}')
     except Exception as e:
