9f431b35b087a8f94e1b9fa34b30d4036ee7facd
Louis <louis.dupraz@epitech.eu>
2023-07-07 20:46:29 +0200

    fix: fix folder deletion command

--- src/task_scheduler.py
+++ src/task_scheduler.py
@@ -1,4 +1,3 @@
-import win32com.client
 from subprocess import run
 
 
@@ -14,14 +13,9 @@ def checkTasksFolderExistsAndDelete(path, skip_prompts):
 
 
 def checkTaskExistsAndDelete(path, skip_prompts):
-    scheduler = win32com.client.Dispatch('Schedule.Service')
-    scheduler.Connect()
     try:
-        task_folder_path, task_name = path.rsplit("\\", 1)
-        task_folder = scheduler.GetFolder(task_folder_path)
-        task = task_folder.GetTask(task_name)
         if skip_prompts or input(f"The task {path} exists but should have been removed. Do you want to delete it? (y/n) ") == 'y':
-            task_folder.DeleteTask(task_name, 0)
+            run(f'schtasks /Delete /TN "{path}" /F', check=True, shell=True)
             print(f' ==> Deleting task {path}')
     except:
         pass
