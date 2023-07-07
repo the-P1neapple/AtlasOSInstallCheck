d9746b0e3b8202dee69fb6994739ce04ef9bd9e1
Louis <louis.dupraz@epitech.eu>
2023-07-07 20:49:13 +0200

    fix: fix folder deletion command

--- src/task_scheduler.py
+++ src/task_scheduler.py
@@ -1,10 +1,14 @@
+import win32com.client
 from subprocess import run
 
 
 def checkTasksFolderExistsAndDelete(path, skip_prompts):
+    scheduler = win32com.client.Dispatch('Schedule.Service')
+    scheduler.Connect()
     try:
+        task_folder = scheduler.GetFolder(path)
         if skip_prompts or input(f"The tasks folder {path} exists but should have been removed. Do you want to delete it? (y/n) ") == 'y':
-            run(f'schtasks /Delete /TN {path}\\* /F', check=True, shell=True)
+            run(fr'schtasks /Delete /TN "{path}\*" /F', check=True, shell=True)
             run(f'schtasks /Delete /TN "{path}" /F', check=True, shell=True)
             print(f' ==> Deleting tasks folder {path}')
     except Exception as e:
@@ -13,7 +17,12 @@ def checkTasksFolderExistsAndDelete(path, skip_prompts):
 
 
 def checkTaskExistsAndDelete(path, skip_prompts):
+    scheduler = win32com.client.Dispatch('Schedule.Service')
+    scheduler.Connect()
     try:
+        task_folder_path, task_name = path.rsplit("\\", 1)
+        task_folder = scheduler.GetFolder(task_folder_path)
+        task = task_folder.GetTask(task_name)
         if skip_prompts or input(f"The task {path} exists but should have been removed. Do you want to delete it? (y/n) ") == 'y':
             run(f'schtasks /Delete /TN "{path}" /F', check=True, shell=True)
             print(f' ==> Deleting task {path}')
