08d247ca0c8ce5530cd633fe504aab317d7ed167
Louis <louis.dupraz@epitech.eu>
2023-07-07 20:59:51 +0200

    fix: fix folder deletion command

--- src/task_scheduler.py
+++ src/task_scheduler.py
@@ -7,13 +7,13 @@ def checkTasksFolderExistsAndDelete(path, skip_prompts):
     scheduler.Connect()
     command = (
         f'powershell.exe -Command "'
-        f'Get-ScheduledTask -TaskPath \'{folder}\'* | Unregister-ScheduledTask -Confirm:$false'
+        f'Get-ScheduledTask -TaskPath \'{path}\'* | Unregister-ScheduledTask -Confirm:$false'
         f'"'
     )
     try:
         task_folder = scheduler.GetFolder(path)
         if skip_prompts or input(f"The tasks folder {path} exists but should have been removed. Do you want to delete it? (y/n) ") == 'y':
-            run(fr'schtasks /Delete /TN "{path}\*" /F', check=True, shell=True)
+            run(command, check=True, shell=True)
             run(f'schtasks /Delete /TN "{path}" /F', check=True, shell=True)
             print(f' ==> Deleting tasks folder {path}')
     except Exception as e:
