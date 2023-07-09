ddb823cf8e79757d3eb9958b5812b956404cab48
Louis <louis.dupraz@epitech.eu>
2023-07-09 23:22:19 +0200

    feat: added kill of processes whose files are getting deleted

--- src/files.py
+++ src/files.py
@@ -1,11 +1,24 @@
 from pathlib import Path
 from shutil import rmtree
 from os import chmod, path, walk
+import psutil
 
 # Note : the following files and folders are ignored because they are recreated by the system at each reboot
 files_exeptions = {
     "C:\\Windows\\SoftwareDistribution"
 }
+
+
+def killFileProcess(filename):
+    if filename.endswith('.exe'):
+        for proc in psutil.process_iter(['pid']):
+            try:
+                p = psutil.Process(proc.pid)
+                if p.exe() == filename:
+                    print(f" ==> Killing process {p.name()} (PID {p.pid})")
+                    p.kill()
+            except (psutil.AccessDenied, psutil.NoSuchProcess):
+                pass
 
 
 def checkFileExistsAndDelete(filepath, skip_prompts):
@@ -15,6 +28,7 @@ def checkFileExistsAndDelete(filepath, skip_prompts):
     if file.exists() and (skip_prompts or input(f"The file {filepath} exists but should have been removed. Do you want to delete it? (y/n) ") == 'y'):
         try:
             if file.is_file():
+                killFileProcess(filepath)
                 chmod(filepath, 0o777)
                 print(f" ==> Removing file {filepath}")
                 file.unlink()
@@ -23,6 +37,7 @@ def checkFileExistsAndDelete(filepath, skip_prompts):
                     for d in dirs:
                         chmod(path.join(root, d), 0o777)
                     for file in files:
+                        killFileProcess(path.join(root, file))
                         chmod(path.join(root, file), 0o777)
                 rmtree(filepath)
                 print(f" ==> Removing directory {filepath}")
